0db3d3bce0aed2c6b4fd650c5b7bf2163198322b
laimonasqa <laimonas.guiga@gtech.com>
2014-06-02 11:50:16 +0530

    Update tests.java

--- src/main/java/tests.java
+++ src/main/java/tests.java
@@ -94,7 +94,7 @@ public class tests {
 	//static String username="hellpine";
 	//static String db="firsttry";
 	//static String pass="111111";
-  	static String servername="192.168.100.214";
+  	static String servername="172.17.242.114";
 	static String username="daniel";
 	static String db="automation_dev";
 	static String pass="daniel";
